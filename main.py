e325d13859c9497c3a7b174a992cb50c20baf338
carlitoselmago <ccarbonell@gmail.com>
2023-08-24 11:48:57 +0200

    first commit

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-import requests
-import wget
-import zipfile
-import os
+#from helpers import getselleniumdriver #this has to be first
+from helpers import savesettings
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 
-# get the latest chrome driver version number
-url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
-response = requests.get(url)
-version_number = response.text
+browser = webdriver.Firefox()
+browser.maximize_window()
+wait = WebDriverWait(browser, 30)
+browser.get('https://twitter.com/login')
 
-# build the donwload url
-download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
+sleep(0.5)
 
-# download the zip file using the url built above
-latest_driver_zip = wget.download(download_url,'chromedriver.zip')
+username_input = wait.until(EC.visibility_of_element_located((By.NAME, "text")))
+username_input.send_keys(user)
 
-# extract the zip file
-with zipfile.ZipFile(latest_driver_zip, 'r') as zip_ref:
-    zip_ref.extractall() # you can specify the destination folder path here
-# delete the zip file downloaded above
-os.remove(latest_driver_zip)
+
+nextbtn=WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(text(), 'Next')]")))
+nextbtn.click()
+
+sleep(1)
+
+#password
+username_input = wait.until(EC.visibility_of_element_located((By.NAME, "password")))
+username_input.send_keys(pwd)
+username_input.send_keys(Keys.ENTER)
+
+sleep(1)
+
+#now get to the liked posts page
+browser.get('https://twitter.com/'+user+'/likes')
+wait = WebDriverWait(browser, 30)
+
+sleep(1)
+wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "article")))
+posts=browser.find_elements(By.CSS_SELECTOR,'div[data-testid="cellInnerDiv"]')
+
+for p in posts:
+    
+    img_elements = p.find_elements(By.TAG_NAME, "img")
+    if img_elements:
+        for img in img_elements:
+            imgsrc=img.get_attribute("src")
+            if "/media/" in imgsrc:
+                print(imgsrc)
+
+
+sleep(60)
