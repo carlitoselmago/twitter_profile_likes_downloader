d1b9ccb58a4358ffede45055641e7fba24f196ff
carlitoselmago <ccarbonell@gmail.com>
2024-07-26 10:49:05 +0200

    added some debugging

--- main.py
+++ main.py
@@ -49,64 +49,71 @@ while True:
     posts=browser.find_elements(By.CSS_SELECTOR,'div[data-testid="cellInnerDiv"]')
 
     for p in posts:
-        #get posturl as id
-        timeelem=p.find_element(By.CSS_SELECTOR,'time[datetime]')
-        parent=timeelem.find_element(By.XPATH, '..')
-        posturl=parent.get_attribute("href")
+        try:
+            #get posturl as id
+            timeelem=p.find_element(By.CSS_SELECTOR,'time[datetime]')
+            parent=timeelem.find_element(By.XPATH, '..')
+            posturl=parent.get_attribute("href")
 
-        print("PROCESS:::",posturl)
+            print("PROCESS:::",posturl)
 
-        if not DB.postexists(posturl):
+            if not DB.postexists(posturl):
 
-            #get userid
-            avatar=p.find_element(By.CSS_SELECTOR,'div[data-testid="Tweet-User-Avatar"]')
-            if avatar:
-                link=avatar.find_element(By.TAG_NAME, "a")
-                if link:
-                    userid=link.get_attribute("href").rsplit('/', 1)[-1]
-                    print(userid)
+                #get userid
+                avatar=p.find_element(By.CSS_SELECTOR,'div[data-testid="Tweet-User-Avatar"]')
+                if avatar:
+                    link=avatar.find_element(By.TAG_NAME, "a")
+                    if link:
+                        userid=link.get_attribute("href").rsplit('/', 1)[-1]
+                        print(userid)
 
-                    #process media
+                        #process media
 
-                    #   images
-                    img_elements = p.find_elements(By.TAG_NAME, "img")
-                    if img_elements:
+                        #   images
+                        img_elements = p.find_elements(By.TAG_NAME, "img")
+                        if img_elements:
 
+                            
+                            for img in img_elements:
+                                imgsrc=img.get_attribute("src")
+                                if "/media/" in imgsrc:
+                                    print(imgsrc)
+                                    try:
+                                        downloadmedia(imgsrc,userid)
+                                        print("")
+                                    except:
+                                        print("could not download media")
+
+                        #   video
+                        video_elements= p.find_elements(By.TAG_NAME, "video")
+                        if video_elements:
+                            print("video element!")
+                            #get post url
+                            
+                            print("posturl",posturl)
+                            try:
+                                #TODO: close the tab if couldn't download video, so move the try to inside the function
+                                download_video(browser,posturl,userid)
+                                sleep(2)
+                                print("")
+                            except Exception as e:
+                                print("could not download video",e)
                         
-                        for img in img_elements:
-                            imgsrc=img.get_attribute("src")
-                            if "/media/" in imgsrc:
-                                print(imgsrc)
-                                try:
-                                    downloadmedia(imgsrc,userid)
-                                    print("")
-                                except:
-                                    print("could not download media")
-
-                    #   video
-                    video_elements= p.find_elements(By.TAG_NAME, "video")
-                    if video_elements:
-                        print("video element!")
-                        #get post url
-                        
-                        print("posturl",posturl)
-                        try:
-                            #TODO: close the tab if couldn't download video, so move the try to inside the function
-                            download_video(browser,posturl,userid)
-                            sleep(2)
-                            print("")
-                        except Exception as e:
-                            print("could not download video",e)
-                    
-            
-            DB.insertpost(posturl)
-        else:
-            print("already saved, skipping....")
+                
+                DB.insertpost(posturl)
+            else:
+                print("already saved, skipping....")
+        except Exception as e:
+            print(":::::::::::::")
+            print("Could not download post:",e)
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
     
     #scroll down to grab more posts
     browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     print("New scroll batch")
-    sleep(4)
+    sleep(6)
 
 print("END:::::::::::::::::::::::::::::::::::::::::::::")
 DB.close()
